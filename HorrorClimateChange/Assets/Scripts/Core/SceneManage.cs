ed5d9acb9aaf0404f6054789388a608b70be7c1a
Srdjan <srkyman@gmail.com>
2019-12-07 18:53:58 +0100

    Scene management system init

--- HorrorClimateChange/Assets/Scripts/Core/SceneManage.cs
+++ HorrorClimateChange/Assets/Scripts/Core/SceneManage.cs
@@ -5,34 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class SceneManage: TGlobalSingleton<SceneManage>
 {
-
     private Scene activeScene;
 
+    private Scene baseScene;
+
+    private const int GAMEPLAY_SCENES_FROM = 2;
+
+    private const int BASE_SCENE = 1;
 
     public void LoadSceneString(string scene, float waitTime = 0f)
     {
         StartCoroutine(LoadScene(scene, waitTime));
     }
 
-    public void LoadSceneInt(int scene, float waitTime = 0f)
+    public void LoadSceneInt(int scene, float waitTime = 0f, LoadSceneMode mode = LoadSceneMode.Additive)
     {
-        StartCoroutine(LoadScene(scene, waitTime));
+        StartCoroutine(LoadScene(scene, waitTime, mode));
     }
     public void UnloadGameScene()
     {
         StartCoroutine(UnloadScene());
     }
 
-    private IEnumerator LoadScene(int i, float waitTime = 0f)
+    private IEnumerator LoadScene(int i, float waitTime = 0f, LoadSceneMode mode = LoadSceneMode.Additive)
     {
         yield return new WaitForSeconds(waitTime);
         AsyncOperation async = new AsyncOperation();
-        async = SceneManager.LoadSceneAsync(i, LoadSceneMode.Additive);
+        async = SceneManager.LoadSceneAsync(i, mode);
         while (!async.isDone)
         {
             yield return null;
         }
         activeScene = SceneManager.GetSceneByBuildIndex(i);
+        if (i == BASE_SCENE)
+        {
+            baseScene = activeScene;
+        }
         SceneManager.SetActiveScene(activeScene);
     }
 
@@ -46,10 +54,14 @@ public class SceneManage: TGlobalSingleton<SceneManage>
             yield return null;
         }
         activeScene = SceneManager.GetSceneByName(i);
+        if (activeScene.buildIndex == BASE_SCENE)
+        {
+            baseScene = activeScene;   
+        }
         SceneManager.SetActiveScene(activeScene);
     }
 
-    public IEnumerator UnloadScene()
+    private IEnumerator UnloadScene()
     {
         if (activeScene.name != "")
         {
@@ -60,8 +72,38 @@ public class SceneManage: TGlobalSingleton<SceneManage>
             {
                 yield return null;
             }
-            activeScene = SceneManager.GetSceneByBuildIndex(0);
+            activeScene = baseScene;
             SceneManager.SetActiveScene(activeScene);
         }
+    }
+
+    // use this
+    public void LoadNextGameplayScene(float wait = 0f)
+    {
+        StartCoroutine(NextScene(wait));
+    }
+
+    private IEnumerator NextScene(float wait)
+    {
+        int nextIndex = activeScene.buildIndex + 1;
+        yield return StartCoroutine(UnloadScene());
+        yield return StartCoroutine(LoadScene(nextIndex, wait));
+    }
+
+    // and this
+    public void LoadGameplay()
+    {
+        // TODO
+        // init gameplay scene with lightning
+        // transitions
+        StartCoroutine(Gameplay());
+    }
+
+    private IEnumerator Gameplay()
+    {
+        int index = GAMEPLAY_SCENES_FROM;
+        yield return StartCoroutine(LoadScene(BASE_SCENE, 0f, LoadSceneMode.Single));
+        // will be additive
+        yield return StartCoroutine(LoadScene(index));
     }
 }
---
+++ HorrorClimateChange/Assets/Scripts/Core/SceneTransitions/SceneTransition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    public enum PlaceToGo
+    {
+        None,
+        PreviousScene,
+        NextScene,
+        SpecialRoom
+    }
+
+    [SerializeField]
+    private PlaceToGo placeToGo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (placeToGo == PlaceToGo.NextScene)
+        {
+            SceneManage.Instance.LoadNextGameplayScene();
+        }
+        else if (placeToGo == PlaceToGo.PreviousScene)
+        {
+            Debug.Log("No turning back!");
+        }
+        else if (placeToGo == PlaceToGo.SpecialRoom)
+        {
+            Debug.Log("Special room not implemented!");
+        }
+    }
+}
